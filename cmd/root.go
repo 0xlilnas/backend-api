356f72bef9ce377a3b57963b5b157af524c4cd50
Naz M <nazlee41@gmail.com>
2022-07-31 16:23:03 +0800

    feat: add start up application details

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli-app",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "memorize",
+	Short: "A tool to help you memorize better",
+	Long:  `To become a super productive person,a good memory helps to make you work faster without any helps.So this app help you imprint the memory in your work flow in your day to day tasks`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
